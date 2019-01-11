6deeb51066f758c69868ad679cdc30efe8e9d168
Nicholas Wease <skynet60@outlook.com>
2019-01-11 19:41:38 +0100

    first commit

---
+++ src/components/header_footer/SideDrawer.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import Drawer from '@material-ui/core/Drawer';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+
+const SideDrawer = (props) => {
+    return (
+        <Drawer
+            anchor="right"
+            open={props.open}
+            onClose={()=> props.onClose(false)}
+        >
+            <List component="nav">
+                <ListItem button onClick={()=> console.log('Featured')}>
+                    Event starts in
+                </ListItem>
+
+                <ListItem button onClick={()=> console.log('Nicks Place INFO')}>
+                    Nick Place INFO
+                </ListItem>
+
+                <ListItem button onClick={()=> console.log('Highlights')}>
+                    Highlights
+                </ListItem>
+
+                <ListItem button onClick={()=> console.log('Prices')}>
+                    Prices
+                </ListItem>
+
+                <ListItem button onClick={()=> console.log('Location')}>
+                    Location
+                </ListItem>
+            </List>
+        </Drawer>
+    );
+};
+
+export default SideDrawer;
