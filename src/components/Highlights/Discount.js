92b9ad4f2e2ae07da6b0f130b450d47a1b1733d2
Nicholas Wease <skynet60@outlook.com>
2019-01-15 10:15:44 +0100

    Continued work of Highlights section

--- src/components/Highlights/Discount.js
+++ src/components/Highlights/Discount.js
@@ -1,10 +1,37 @@
 import React, {Component} from 'react';
+import Fade from 'react-reveal/Fade';
+import Slide from 'react-reveal/Slide';
 
 class Discount extends Component {
+
+    state = {
+        discountStart: 0,
+        discountEnd: 25
+    };
+
+    percentage = () => {
+
+    };
+
     render() {
         return (
-            <div>
-                SOMETHING
+            <div className="center_wrapper">
+                <div className="discount_wrapper">
+                    <Fade onReveal={() => this.percentage()}>
+                        <div className="discount_percentage">
+                            <span>25%</span>
+                            <span>OFF</span>
+                        </div>
+                    </Fade>
+
+                    <Slide right>
+                        <div className="discount_description">
+                            <h3>Purchase Tickets now before they are gone!!!</h3>
+                            <p>Complete strangers can stand silent next to each other in an elevator and not even look each other in the eye. But at a concert, those same strangers could find themselves dancing and singing together like best friends. That's the power of music.</p>
+                            <div>BUTTON</div>
+                        </div>
+                    </Slide>
+                </div>
             </div>
         );
     }
